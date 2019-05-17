c6c59854efd669d2810ddb29a09e54b865418134
alyssalea <alyssalea@icloud.com>
2019-05-16 22:59:41 -0400

    Add files via upload

---
+++ willsolitrobot/sketch.js
@@ -0,0 +1,31 @@
+
+function setup() {
+  createCanvas(800,800)
+  frameRate(8)
+  background(0,255,145)
+  
+}
+function draw( ) {
+  fill(random(400,156),random(146,255),random(0,255))
+  ellipse(random(0,800),random(0,800),random(0,200))
+  ellipse(400,200,100,100)
+  rect(350,250,100,200);
+  rect(350,450,25,100)
+rect(425,450,25,100)
+  
+  fill(255,0,0)
+  ellipse(400,200,40,40)
+  
+  fill(255,0,0)
+  ellipse(400,400,40,40)
+  
+   fill(255,0,0)
+  ellipse(400,325,40,40)
+  
+  ellipse(360,550,40)
+  ellipse(440,550,40)
+  fill(random(400,156),random(146,255),random(0,255));
+  rect(350,150,100);
+  
+}
+
